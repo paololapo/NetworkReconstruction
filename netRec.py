b17d5d8b97226560dafc9be6b296ac908ec8c0b3
paololapo <paololapocerni@gmail.com>
2024-08-23 18:17:59 +0200

    pre massive run

--- netRec.py
+++ netRec.py
@@ -10,18 +10,19 @@ import multiprocessing
 from time import time
 import pickle
 from pprint import pprint
+from datetime import datetime
 
 from methods import *
 
 # Florentine Families Network
 G = nx.florentine_families_graph().to_undirected()
 print("Florentine Families Network")
-#studyNetworkReconstructon(G, log_path="./logs/florentine_families.pkl",
-#                          n_samples=1e4, 
-#                          delay=50, 
-#                          transient=100, 
-#                          offset=120, 
-#                          verbose=True)
+studyNetworkReconstructon(G, log_path="./logs/florentine_families.pkl",
+                         n_samples=1e4, 
+                         delay=50, 
+                         transient=100, 
+                         offset=120, 
+                         verbose=True)
 
 # Karate Club Network
 G = nx.karate_club_graph().to_undirected()
